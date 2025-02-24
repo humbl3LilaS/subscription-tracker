2366ee91c31fe34aaa4c788df20897788e0a7629
humbl3LilaS <sabishinekobebe@gmail.com>
2025-02-24 13:15:36 +0630

    add: Error Handling Middleware and other utility middle to app

--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
 import express from "express";
+
 import { config } from "./config/env.ts";
 import { authRouter } from "./routes/auth.ts";
 import { usersRouter } from "./routes/users.ts";
 import { subscriptionsRouter } from "./routes/subscriptions.ts";
 import { connectToDB } from "./database/db.ts";
+import cookieParser from "cookie-parser";
+import { errorMiddleWare } from "./middlewares/error.ts";
 
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/subscriptions", subscriptionsRouter);
@@ -14,6 +23,9 @@ app.use("/api/v1/subscriptions", subscriptionsRouter);
 app.get("/", (_req, res) => {
     res.send("Welcome to the Subscription Tracker Api");
 });
+
+// Error Handling Middleware
+app.use(errorMiddleWare);
 
 app.listen(config.env.PORT || 3000, async () => {
     console.log(`Server Listening on port http://localhost:${config.env.PORT || 3000}/`);
